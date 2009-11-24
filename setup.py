7dd03af95af284abb085dfbf1a863c1bb7287329
Michał Łowicki <mlowicki@opera.com>
2009-11-24 15:19:24 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-js-builder",
+    version="0.1",
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=["setuptools"],
+)
+
